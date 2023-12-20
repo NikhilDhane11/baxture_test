4b80224fdc2ff01bd2a84a6d164f7e1d185a0710
Nikhil Dhane <nikhil.dhane11@gmail.com>
2023-12-20 09:08:38 +0530

    cluster file changes

--- cluster.js
+++ cluster.js
@@ -1,23 +1,3 @@
-// // cluster.js
-// const cluster = require('cluster');
-// const os = require('os');
-
-// if (cluster.isMaster) {
-//   const numCPUs = os.cpus().length - 1;
-
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
-
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.log(`Worker ${worker.process.pid} died`);
-//     cluster.fork();
-//   });
-// } else {
-//   require('./server.js');
-// }
-
-
 const cluster = require('cluster');
 const os = require('os');
 
